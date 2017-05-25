6edb41080e05539b134cd6bb9c3dd4c4a4004b0d
vignaesh01 <vignaesh01@gmail.com>
2017-05-25 09:39:18 +0530

    admobs plugin code changes

--- www/js/index.js
+++ www/js/index.js
@@ -33,9 +33,10 @@ var app = {
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
-        app.receivedEvent('deviceready');
+        //app.receivedEvent('deviceready');
+		  document.removeEventListener('deviceready', onDeviceReady, false);
 		 admob.setOptions({
-        publisherId:          "ca-app-pub-7830155450711523/8193228147",  // Required
+			publisherId:  "ca-app-pub-7830155450711523/8193228147" 
      
       
       });
